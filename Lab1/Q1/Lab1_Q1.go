053c3dbd59d2e6207ce4e613b50f4e0ccba836b4
Pr1me9 <232309550+Abh1nav90@users.noreply.github.com>
2026-09-05 13:53:38 +0530

    Add files via upload

---
+++ Lab1/Q1/Lab1_Q1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Abhinav Jha")
+	fmt.Println("Integer Operation")
+	a := 15
+	b := 4
+
+	fmt.Printf("Addition: %d \n", a+b)
+	fmt.Printf("Substraction: %d \n", a-b)
+	fmt.Printf("Multiplication: %d \n", a*b)
+	fmt.Printf("Division: %d \n", a/b)
+
+	fmt.Println("Float Operation")
+	c := 10.5
+	d := 2.0
+
+	fmt.Printf("Addition: %f \n", c+d)
+	fmt.Printf("Substraction: %f \n", c-d)
+	fmt.Printf("Multiplication: %f \n", c*d)
+	fmt.Printf("Division: %f \n", c/d)
+}
